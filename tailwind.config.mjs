3878126d6247b22a2f3db7ae48501d77187a7c6b
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-07-24 22:43:45 -0300

    Added smx screen size

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -59,6 +59,9 @@ module.exports = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      screens: {
+        smx: '480px'
+      },
       keyframes: {
         'accordion-down': {
           from: { height: '0' },
