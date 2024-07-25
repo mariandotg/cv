4b18b0f10fb5c7f3755e2d540eb63d0f1ac59630
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-07-25 00:13:22 -0300

    Added GA and sitemap scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 import { defaultLocale, localesArray } from "./src/data/locales";
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://cv.marianoguillaume.com',
+  integrations: [tailwind(), react(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   i18n: {
     defaultLocale,
     locales: localesArray
