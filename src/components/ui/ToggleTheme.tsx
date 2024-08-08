420dc6da57b550f53beedf6bab2f3ecd3f318246
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-08-08 19:48:07 -0300

    Fixed ToggleTheme component

--- src/components/ui/ToggleTheme.tsx
+++ src/components/ui/ToggleTheme.tsx
@@ -4,90 +4,26 @@ import { DarkModeIcon } from '../icons/DarkModeIcon'
 import { LightModeIcon } from '../icons/LightModeIcon'
 
 export function ToggleTheme() {
-  const [theme, setTheme] = useState<string>(
-    typeof window !== 'undefined'
-      ? localStorage.theme !== undefined
-        ? localStorage.theme
-        : 'dark'
-      : 'dark',
-  )
-  // const [isMounted, setIsMounted] = useState(true)
-  // console.log('state ', isMounted)
-  // console.log('render')
-  // useEffect(() => {
-  //   console.log('primer render')
-  //   console.log('isMounted: ', isMounted)
-  //   setIsMounted((prev) => !prev)
+  const [theme, setTheme] = useState<string>('dark')
 
-  //   console.log('se actualizó isMounted')
-  // }, [])
-  const getTheme = () => {
-    if (typeof localStorage !== 'undefined') {
-      const storedTheme = localStorage.getItem('theme')
-      if (storedTheme) {
-        return storedTheme
-      }
-    }
-    if (
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      return 'dark'
-    }
-    return 'light'
-  }
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme')
+    const defaultTheme =
+      storedTheme ||
+      (window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light')
+    setTheme(defaultTheme)
+    document.documentElement.classList.toggle('dark', defaultTheme === 'dark')
+  }, [])
+
   const handleClick = () => {
-    console.log('click')
-    setTheme(theme === 'light' ? 'dark' : 'light')
-    let themeLocal = getTheme()
-    themeLocal = theme === 'dark' ? 'light' : 'dark'
-    console.log('test apply theme')
-    localStorage.setItem('theme', theme)
-
-    // if(firstTime) {
-    // 	if (theme === 'dark') {
-    // 		document.documentElement.classList.add('dark')
-    // 	} else {
-    // 		document.documentElement.classList.remove('dark')
-    // 	}
-    // } else {
-
-    // document.startViewTransition(() => {
-    console.log('view transition')
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-    // })
-    // }
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+    localStorage.setItem('theme', newTheme)
+    document.documentElement.classList.toggle('dark', newTheme === 'dark')
   }
 
-  // useEffect(() => {
-  //   document.startViewTransition(() => {
-  //     console.log('view transition')
-
-  //     if (theme === 'dark') {
-  //       document.documentElement.classList.add('dark')
-  //     } else {
-  //       document.documentElement.classList.remove('dark')
-  //     }
-  //   })
-  //   localStorage.setItem('theme', theme)
-  // }, [theme])
-
-  //   const toggler = document.getElementById('themeToggler')
-
-  //   toggler?.addEventListener('click', () =>
-  //     document.startViewTransition(() => {
-  //       console.log('view transition')
-  //       document.documentElement.classList.toggle('dark')
-  //     }),
-  //   )
-  // if (!isMounted) {
-  //   return <div>test</div> // or null;
-  // }
-  console.log(theme)
   return (
     <Button
       onClick={handleClick}
